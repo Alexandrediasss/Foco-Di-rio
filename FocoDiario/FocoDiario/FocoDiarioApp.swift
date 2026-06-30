7d141a2e57424a850668c046e68799ab13f559c9
Carlos Alexandre Dias <alexandreprofissional2005@gmail.com>
2026-06-30 16:16:22 -0400

    Add to Xcode

---
+++ FocoDiario/FocoDiario/FocoDiarioApp.swift
@@ -0,0 +1,32 @@
+//
+//  FocoDiarioApp.swift
+//  FocoDiario
+//
+//  Created by Carlos Alexandre Dias Messias de Lima on 30/06/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FocoDiarioApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
